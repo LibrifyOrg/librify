07526200242bc490c9d4a532c171364a9be6b392
JortvD <10116429+JortvD@users.noreply.github.com>
2019-07-16 23:49:21 +0200

    Add custom webpack building

---
+++ build/build.dev.js
@@ -0,0 +1,14 @@
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.dev.conf");
+
+const startTime = process.hrtime();
+
+console.log("Building the webapp");
+
+webpack(webpackConfig, err => {
+	if(err) throw err;
+
+	const endTime = process.hrtime(startTime);
+
+	console.log("Build it successfully in %ds and %dms", endTime[0], endTime[1] / 1000000);
+});
