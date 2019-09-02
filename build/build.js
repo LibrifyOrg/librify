56ecf0a71aa4726dabe64c1dfc7b4f2e2b7a17f2
JortvD <10116429+JortvD@users.noreply.github.com>
2019-09-02 20:28:48 +0200

    Update the build file

--- build/build.js
+++ build/build.js
@@ -1,14 +1,30 @@
 const webpack = require("webpack");
+const path = require("path");
+const NodeUtils = require("util");
+const packager = require("electron-packager");
 const webpackConfig = require("./webpack.prod.conf");
 
-const startTime = process.hrtime();
+const packagerOptions = {
+	dir: path.resolve(__dirname, "../"),
+	out: path.resolve(__dirname, "../dist"),
+	platform: "all"
+};
+let startTime = process.hrtime();
 
-console.log("Building the webapp");
+console.log("Building with webpack");
 
-webpack(webpackConfig, err => {
-	if(err) throw err;
-
-	const endTime = process.hrtime(startTime);
+NodeUtils.promisify(webpack)(webpackConfig)
+.then(() => {
+	let endTime = process.hrtime(startTime);
 
 	console.log("Build it successfully in %ds and %dms", endTime[0], endTime[1] / 1000000);
-});
+	startTime = process.hrtime();
+	console.log("Building into executable");
+
+	return packager(packagerOptions);
+})
+.then(() => {
+	endTime = process.hrtime(startTime);
+	console.log("Build it successfully in %ds and %dms", endTime[0], endTime[1] / 1000000);
+})
+.catch(err => {throw err});
