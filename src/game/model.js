1b1dca1e093b9a2c3682e604a6d2ca403742e20c
JortvD <10116429+JortvD@users.noreply.github.com>
2019-09-12 21:36:18 +0200

    Add more data to the game model

--- src/game/model.js
+++ src/game/model.js
@@ -3,14 +3,20 @@ export default class Game {
 		this.id = config.id;
 		this.name = config.name;
 		this.origin = config.origin;
-		this.sources = config.sources.map(source => app.gameManager.sourceManager.create(source));
+		this.background = config.background;
+		this.icon = config.icon;
+		this.banner = config.banner;
+		this.sources = config.sources.map(source => app.gameManager.sourceManager.create(source)).filter(source => source !== undefined);
 	}
 
 	toObject() {
 		let obj = {
 			id: this.id,
 			name: this.name,
-			origin: this.origin
+			origin: this.origin,
+			background: this.background,
+			icon: this.icon,
+			banner: this.banner
 		};
 
 		obj.sources = this.sources.map(source => source.toObject());
