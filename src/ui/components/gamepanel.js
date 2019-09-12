b03160765b061654016d484c8244e01bf1320188
JortvD <10116429+JortvD@users.noreply.github.com>
2019-09-12 21:35:11 +0200

    Add the panel component

---
+++ src/ui/components/gamepanel.js
@@ -0,0 +1,15 @@
+import m from "mithril";
+
+export default class GamePanelComponent {
+	oninit(vnode) {
+		this.app = vnode.attrs.app;
+	}
+
+	view(vnode) {
+		let panel = this.app.gameManager.panelManager.get(vnode.attrs.type);
+
+		if(panel === undefined) return (<div class="panel" id="panel"></div>);
+
+		return (<div class="panel" id="panel">{panel.component(vnode.attrs.game)}</div>);
+	}
+}
