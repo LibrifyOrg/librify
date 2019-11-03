b0928786c9adb1eb73183f8012eb924f9c3202ac
JortvD <10116429+JortvD@users.noreply.github.com>
2019-11-03 10:57:23 +0100

    Add forced fetching to the config manager

--- src/config/manager.js
+++ src/config/manager.js
@@ -5,17 +5,24 @@ import FileAsync from "lowdb/adapters/FileSync";
 export default class ConfigManager extends Map {
 	constructor() {
 		super();
+
+		this.configPath = "resources/config/";
 	}
 
 	async get(name) {
 		if(this.has(name)) return super.get(name);
 
-		const configPath = path.resolve("resources/config/" + name);
+		const configPath = path.resolve(this.configPath + name);
 		const adapter = new FileAsync(configPath);
 		const config = await low(adapter);
 
 		this.set(name, config);
 
 		return config;
+	}
+	forceGet(name) {
+		if(this.has(name)) this.delete(name);
+
+		return this.get(name)
 	}
 }
