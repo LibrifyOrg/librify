a024adb5d0168984018de81ec5f8749807d03006
JortvD <10116429+JortvD@users.noreply.github.com>
2019-11-03 10:50:30 +0100

    Remove the window handler from the app class

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import electron from "electron";
 import UIHandler from "@/ui/handler";
-import WindowHandler from "@/ui/window";
 import GameManager from "@/game/manager";
 import ConfigManager from "@/config/manager";
 import PluginManager from "@/plugin/manager";
@@ -9,7 +8,6 @@ import { EventEmitter } from "events";
 export default class Application extends EventEmitter {
 	constructor() {
 		this.uiHandler = new UIHandler(this);
-		this.windowHandler = new WindowHandler(this);
 		this.configManager = new ConfigManager(this);
 		this.pluginManager = new PluginManager(this);
 		this.gameManager = new GameManager(this);
