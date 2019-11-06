4408ff54046df043b3a513a3d80ebc99dca0fbc2
JortvD <10116429+JortvD@users.noreply.github.com>
2019-11-06 01:06:04 +0100

    Disable the nagging security warning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import {remote} from "electron";
 import Application from "@/app";
 
+delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+
 const app = new Application();
 app.start();
 
