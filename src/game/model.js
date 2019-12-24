7ebaf71af6be1039f96f85937c9f73fd6cf849db
JortvD <10116429+JortvD@users.noreply.github.com>
2019-12-24 23:46:41 +0100

    Add populate function to game model

--- src/game/model.js
+++ src/game/model.js
@@ -1,5 +1,6 @@
 export default class Game {
 	constructor(app, data) {
+		this.app = app;
 		this.data = Object.assign(...Array.from(app.games.dataTypes.values()).map(dataType => dataType.default()), data);
 
 		for(let [key, dataType] of Array.from(app.games.dataTypes.entries())) {
@@ -7,18 +8,26 @@ export default class Game {
 		}
 	}
 
-	get id() {
-		return this.data.id;
+	populate(object, override = true) {
+		let keys = Object.keys(object);
+
+		for(let key of keys) {
+			if((this.data[key] !== undefined && !override) || protectedKeys.includes(key)) {
+				continue;
+			}
+
+			this.data[key] = object[key];
+		}
 	}
 
-	set id(value) {
-		this.data.id = value;
+	get id() {
+		return this.data.id;
 	}
 
 	toObject() {
 		let obj = this.data;
 
-		for(let [key, dataType] of Array.from(app.games.dataTypes.entries())) {
+		for(let [key, dataType] of Array.from(this.app.games.dataTypes.entries())) {
 			const value = dataType.toObject(this);
 
 			if(value !== undefined) obj[key] = value;
