08a344fb17bc817f2d1a7efd0921839f5b9f6f00
JortvD <10116429+JortvD@users.noreply.github.com>
2019-12-24 23:41:59 +0100

    Change logging for the dev build script

--- build/build.dev.js
+++ build/build.dev.js
@@ -3,12 +3,10 @@ const webpackConfig = require("./webpack.dev.conf");
 
 const startTime = process.hrtime();
 
-console.log("Building the webapp");
-
 webpack(webpackConfig, err => {
 	if(err) throw err;
 
 	const endTime = process.hrtime(startTime);
 
-	console.log("Build it successfully in %ds and %dms", endTime[0], endTime[1] / 1000000);
+	console.log("Build the webapp successfully %ds and %dms after start", endTime[0], endTime[1] / 1000000);
 });
